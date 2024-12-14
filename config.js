e9c0ae92da73e98ba2260cdda0093fb12835bc9f
Darksadas YT <sadaspersonal@gmail.com>
2024-12-14 12:02:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: 'DARK-SHUTER-MD=SZ8wxTpC#Ql7fXgaC3q1sJqcZcjYljHFyMv1uMOcHw0UsQj5ujas',
-GITHUB_AUTH_TOKEN: '5hpvVCXwsX1filbJvOUVXuLcDGx3vh31dclt', //"ghp_ " මෙම කොටස ඉවත් කර token එක දාන්න.
-GITHUB_USER_NAME: 'DarksadasYT1',
+SESSION_ID: 'put session',
+GITHUB_AUTH_TOKEN: 'put token', //"ghp_ " මෙම කොටස ඉවත් කර token එක දාන්න.
+GITHUB_USER_NAME: 'put your github user name',
 
 };
