2035e5938eecc6c90993efab1b7ac6dbd4140b7f
yffox <prabodgayantha26@outlook.com>
2024-12-16 09:08:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: 'put session',
-GITHUB_AUTH_TOKEN: 'put token', //"ghp_ " මෙම කොටස ඉවත් කර token එක දාන්න.
-GITHUB_USER_NAME: 'put your github user name',
+SESSION_ID: 'DARK-SHUTER-MD=DEFAARBa#sP6eAubuCoquES3G0tohRGuRZuTJz3dDjlTKz1XGTEw',
+GITHUB_AUTH_TOKEN: 'sm0KxVgf006JXaq63AzeC7V4N5B1dC1oZN2l', //"ghp_ " මෙම කොටස ඉවත් කර token එක දාන්න.
+GITHUB_USER_NAME: 'yffox',
 
 };
